58ac7ceeba9fea5e5fe750f5b7c347983b59e17c
yogesh kumar <ykumar@eteaminc.com>
2023-11-10 00:35:46 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,233 @@
+// let arr = [1,2,3,4,5,6]
+
+// let sl = arr.splice(2,2) (2 index se 2 element hta do) (it alters the array)
+// console.log(sl,arr)
+
+// let sl = arr.slice(2,4) (2 index se 4 index tak(usme bhi 4th index not included)) (it does not alter the array)
+// console.log(sl,arr)
+
+// const fib = (n) => {
+//     let arr = [0,1]
+
+//     for(let i = 2; i<=n; i++)
+//     {
+//         arr.push(arr[i-1] + arr[i-2])
+//     }
+//     console.log(arr.join(','))
+// }
+
+// fib(12)
+
+// const anagram = (str1,str2) => {
+//     console.log(str1.length)
+//     if(str1.length !== str2.length) return false
+//     let s1 = str1.split('').sort().join('')
+//     let s2 = str2.split('').sort().join('')
+//   return  s1 === s2
+// }
+
+// console.log(anagram('car','arc'))
+
+// const firstSum = (arr,num) => {
+//     for(let i = 0; i< arr.length; i++)
+//     {
+//         for(let j = 1; j< arr.length; j++){
+//             // console.log(arr[i] + arr[j])
+//             if(arr[i] + arr[j] == num) {
+//                 console.log('i='+ i +',j='+j)
+//               return console.log([i,j])
+//             }
+//         }
+//     }
+// }
+
+// let arr = [1,2,3,8,1,3]
+
+// firstSum(arr,9)
+
+// const maxProfit = (arr) => {
+//     let globalProfit = 0
+
+//     for(let i = 0; i< arr.length -1; i++){
+//         for(let j = i+1; j < arr.length; j++){
+//             let currentProfit =  arr[j] - arr[i]
+//             if(currentProfit > globalProfit)  globalProfit = currentProfit
+//         }
+//     }
+//     console.log(globalProfit)
+// }
+
+// maxProfit([5,4,3,22,11])
+
+// const rotateArray = (arr,k) => {
+
+//     let rotate = arr.splice(arr.length - k, k)       [1,2,3,4,5,6,7,8]
+//     console.log([...rotate,...arr])
+// }
+
+// rotateArray([1,2,3,4,5,6,7,8,9],9)
+
+// const calculateUniqNo = (arr) => {
+//     let count = 0
+
+//     arr.map((elem,idx) => {
+//       if (arr.indexOf(elem) == idx) count++
+//     })
+
+//     console.log(count)
+
+// }
+// calculateUniqNo([0,0,0,0,0,11,1,1,2,2,3,3,3,4])
+
+//find the sub array with largest sum
+
+// const   = (arr) => {
+//     let sum = arr[0]
+//     let strIdx, endIdx
+
+//     for(let i = 0; i< arr.length; i++){
+//         let currentSum = 0
+//         for(let j = i; j< arr.length; j++){
+//             currentSum = currentSum + arr[j]
+
+//             if(currentSum > sum) {
+//                 sum = currentSum
+
+//                 strIdx = i
+//                 endIdx = j
+//             }
+//         }
+//     }
+//     console.log(sum)
+//      console.log(arr.slice(strIdx, endIdx+1))
+// }
+
+// subArray([-2,1,3,-1,4,2,3,1])
+
+// const revWords = (str) => {
+//      let strArr = str.trim().split(" ").reverse()
+//      console.log(strArr)
+//      let stack = []
+//      for(let i of strArr) stack.push(i)
+//      console.log(stack.join(' ').trim())
+// }
+
+// revWords("  Hello  World   ")
+
+// const checkBrackets = (str) => {
+
+//     let stack = []
+// for(let i = 0; i< str.length; i++){
+//     let char = str[i]
+//     if(char == '(' || char == '{' || char == '['){
+//         stack.push(char)
+//     }
+//     else if(char == ')' || char == '}' || char == ']'){
+//         if(isEmpty(stack)){
+//             return false
+//         }
+//         let top = stack.pop()
+//         if((char == '(' && char !== ')') || (char == '{' && char !== '}') || (char == '[' && char !== ']')){
+//             return false
+//         }
+//     }
+// }
+// return isEmpty(stack)
+
+// }
+
+// const isEmpty = (stack) => {
+// return stack.length === 0
+// }
+
+// const string1 = "([{}])";
+// console.log(checkBrackets(string1))
+
+// const slidingWindow = (arr,k) => {
+//     let res = []
+//     for(let i =0; i<= arr.length - k; i++){
+//         let max = arr[i]
+//         for(let j = 1; j< k; j++){
+//             if(arr[j+i] > max){
+//                 max = arr[j+i]
+//             }
+//         }
+//         res.push(max)
+//     }
+//     console.log(res)
+// }
+
+// slidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3)
+
+// const checkHamming = (str1,str2) => {
+//     str1 = str1.toString(2)
+//     str2 = str2.toString(2)
+//     if(str1.length < str2.length) {
+//         while(str1.length !== str2.length) str1 = '0'+str1
+//     }
+//     else if(str1.length > str2.length) {
+//         while(str1.length !== str2.length) str2 = '0'+str2
+//     }
+//     let count = 0
+//     for(let i = 0; i < str1.length ; i++){
+//         if(str1[i] !== str2[i]) count ++
+//     }
+//     return count
+// }
+// console.log(checkHamming('hello','asddf'))
+
+// let arr = [1,2,3,4,4,3,2,1]
+// let unq = new Set([...arr])
+// unq.add('fgfghfgh')
+// console.log(unq)
+// console.log(unq.size)
+
+// const lenghtOfLongestSubstring = (s) => {
+//     if(!s){
+//         return 0
+//     }
+
+//     let end = 0; start = 0; maxLength = 0,startIdx = 0,lastIdx = 0
+//     const unq = new Set()
+//     while(end < s.length){
+//         if(!unq.has(s[end])){
+//             unq.add(s[end])
+//             end++
+//             maxLength = Math.max(maxLength, unq.size)
+//             startIdx = start
+//             lastIdx = end
+//         }
+//         else{
+//             unq.delete(s[start])
+//             start++
+//         }
+//     }
+//     console.log(maxLength,startIdx,lastIdx)
+// }
+
+// lenghtOfLongestSubstring('pwwkew')
+
+// Sort colour
+// var sortColors = function(nums) {
+//     let zero = 0
+//     let one = 0
+//     let two = 0
+//     let last = nums.length
+ 
+//     for(let  i of nums){
+//         if(i === 0) zero++
+//         else if(i === 1) one++
+//         else two++
+//     }
+//     for(let j = 0; j< last;j++){
+//        if(j<zero)
+//            nums[j] = 0
+       
+//        else if(j >= zero && j < (zero+one)) nums[j] = 1
+//        else nums[j] = 2
+       
+//     }
+//     return nums
+ 
+//  };
+// sortColors([2,0,2,1,1,0])
---
+++ object.js
@@ -0,0 +1,185 @@
+//Comparing a object
+
+// let obj = { name: "Siraj" };
+// let obj2 = { name: "Siraj" };
+
+// let arr = ["Sami", "Rohit", "Sami", "Virat", obj];
+// let arr2 = ["Hardik", "Rohit", "Sami", "Kohli", obj2];
+
+// const checkObj = (arr) => {
+//   return arr.map((elem) => {
+//     if (typeof elem == "object" || elem !== null) {
+//       return JSON.stringify(elem);
+//     }
+//     return elem;
+//   });
+// };
+
+// let newArr = [...checkObj(arr), ...checkObj(arr2)];
+
+// console.log(new Set([...newArr]));
+
+// Sorting array of objects
+
+// let obj = [
+//   {
+//     name: "Rohit",
+//   },
+//   {
+//     name: "Siraj",
+//   },
+//   {
+//     name: "Virat",
+//   },
+//   {
+//     name: "Ayer",
+//   },
+// ];
+
+// let sorting = obj.sort((a, b) => {
+//   if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+//   else return -1;
+// });
+
+// console.log("sorting", sorting);
+
+// let obj = {
+//   name1: "Rohit",
+//   name2: "Virat",
+// };
+
+// let objArr = Object.entries(obj);
+// console.log("objArr", objArr);
+
+// let start = [
+//   {
+//     id: 1,
+//     status: "completed",
+//     name: "The Lord of the Rings",
+//   },
+//   {
+//     id: 2,
+//     status: "in-progress",
+//     name: "The Lord of the Flies",
+//   },
+//   {
+//     id: 3,
+//     status: "not-started",
+//     name: "Dune",
+//   },
+//   {
+//     id: 4,
+//     status: "not-started",
+//     name: "American Gods",
+//   },
+//   {
+//     id: 5,
+//     status: "completed",
+//     name: "Enders Game",
+//   },
+//   {
+//     id: 6,
+//     status: "in-progress",
+//     name: "Brave new world",
+//   },
+//   {
+//     id: 7,
+//     status: "completed",
+//     name: "1984",
+//   },
+// ];
+
+// let begin = start.reduce(
+//   (acc, elem) => {
+//     if (elem.status == "completed") {
+//       acc[0].books = [...acc[0].books, { id: elem.id, name: elem.name }];
+//       return acc;
+//     } else if (elem.status == "in-progress") {
+//       acc[1].books = [...acc[1].books, { id: elem.id, name: elem.name }];
+//       return acc;
+//     } else if (elem.status == "not-started")
+//       acc[2].books = [...acc[2].books, { id: elem.id, name: elem.name }];
+//     return acc;
+//   },
+//   [
+//     { status: "completed", books: [] },
+//     { status: "in-progress", books: [] },
+//     { status: "not-started", books: [] },
+//   ]
+// );
+
+// begin.map((elem) =>
+//   elem.books.sort((a, b) => {
+//     return a.name.toString().toLowerCase() > b.name.toString().toLowerCase()
+//       ? 1
+//       : -1;
+//   })
+// );
+// console.log("begin", begin);
+
+// let obj = {
+//   a: 2,
+//   b: 3,
+//   c: 4,
+// };
+// let sum = 0;
+
+// for (i in obj) {
+//   console.log("i", i);
+//   obj[i] = obj[i] * 2;
+//   sum = sum + obj[i]; //it gets updated value for adding
+// }
+// console.log("obj", obj, sum);
+
+// let valueSum = Object.values(obj).reduce((acc, elem) => (acc = acc + elem), 0);
+// console.log("valueSum", valueSum);
+
+//Object.freeze({})
+
+// function show(val) {
+//   ++val;
+//   console.log("val", val); // 5
+// }
+
+// let val = 4;
+// show(val);
+// console.log("val2", val); // 4
+
+// function show2(arr) {
+//   arr.push(7); // array is an object so it mutates the value
+//   console.log("val", arr);
+// }
+
+// let arr = [1, 2, 3];
+// show2(arr);
+// console.log("val2", arr);
+
+// function show2(arr1, arr2) {
+//   arr1 = arr2; //here array is not mutated, it is assigned a new value
+//   console.log("arr1", arr1);
+// }
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [7, 8, 9];
+// show2(arr1, arr2);
+// console.log("val2", arr1);
+// console.log(show2.name);
+// console.log(show2.length);
+
+// const getMaxChar = (str) => {
+//   let strArr = str.split('')
+//   let obj = {}
+//   strArr.forEach((elem) => obj[elem] = obj[elem] ? obj[elem]+ 1 : 1)
+// console.log(obj)
+// let max = 0
+// let char = ''
+// for(let i in obj){
+//   if(obj[i] > max){
+//       max = obj[i]
+//       char = i
+//   }
+// }
+// console.log(char,max)
+// }
+
+// getMaxChar("Hello World")
